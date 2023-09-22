93432f503206ea91a5ca3da701586b99f60286a6
oumaima1115 <oumaima.ayachi@esprit.tn>
2023-09-22 12:59:36 +0100

    create a project intellij

---
+++ src/main/java/com/example/candidat/CandidatApplication.java
@@ -0,0 +1,13 @@
+package com.example.candidat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidatApplication.class, args);
+    }
+
+}
