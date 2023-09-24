2afa4f48187ee75bd2004ff02c5ca615fffd3ff3
oumaima1115 <oumaima.ayachi@esprit.tn>
2023-09-24 09:33:38 +0100

    Add Service Candidat

--- src/main/java/com/example/candidat/CandidatRestApi.java
+++ src/main/java/com/example/candidat/CandidatRestApi.java
@@ -1,7 +1,10 @@
 package com.example.candidat;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CandidatRestApi {
@@ -10,5 +13,26 @@ public class CandidatRestApi {
     public String sayHello(){
         System.out.println(title);
         return title;
+    }
+    @Autowired
+    private CandidatService candidatService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Candidat> createCandidat(@RequestBody Candidat candidat) {
+        System.out.println("hello");
+        return new ResponseEntity<>(candidatService.addCandidat(candidat), HttpStatus.OK);
+    }
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Candidat> updateCandidat(@PathVariable(value = "id") int id,
+                                                   @RequestBody Candidat candidat){
+        return new ResponseEntity<>(candidatService.updateCandidat(id, candidat),
+                HttpStatus.OK);
+    }
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteCandidat(@PathVariable(value = "id") int id){
+        return new ResponseEntity<>(candidatService.deleteCandidat(id), HttpStatus.OK);
     }
 }
